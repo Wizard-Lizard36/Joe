235b35e0ebe734a173200d9e6715ea89d07c4142
Wizard-Lizard36 <max@bartle.co.nz>
2026-03-18 14:56:49 +1300

    Add PowerShell script to download and run Joe

---
+++ joe.ps1
@@ -0,0 +1,1 @@
+curl -L -o j.zip "https://github.com/Wizard-Lizard36/Joe/archive/refs/heads/main.zip" && powershell Expand-Archive -Path j.zip -DestinationPath joevir && cd joevir/Joe-main\j\joe &&start r.vbs 
